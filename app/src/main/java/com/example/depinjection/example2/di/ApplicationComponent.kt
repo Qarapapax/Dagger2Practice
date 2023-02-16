03c70368ce11a3571eed5f5befe8d89afe0ad6a2
Qarapapax <aldunsar@yahoo.com>
2023-02-16 13:45:55 +0300

    les9

--- app/src/main/java/com/example/depinjection/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/depinjection/example2/di/ApplicationComponent.kt
@@ -1,24 +1,32 @@
 package com.example.depinjection.example2.di
 
 import android.content.Context
+import com.example.depinjection.example2.data.database.ExampleDatabase
+import com.example.depinjection.example2.data.network.ExampleApiService
+import com.example.depinjection.example2.presentation.ExampleViewModel
 import com.example.depinjection.example2.presentation.MainActivity
+import dagger.Binds
 import dagger.BindsInstance
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [DataModule::class, DomainModule::class])
 interface ApplicationComponent {
 
+    fun getViewModel(): ExampleViewModel
+
+    fun getDataBase(): ExampleDatabase
+
+    fun getApiService(): ExampleApiService
+
     fun inject(activity: MainActivity)
 
-    @Component.Builder
-    interface ApplicationComponentBuilder {
-
-        @BindsInstance
-        fun context(context: Context): ApplicationComponentBuilder
-
-        @BindsInstance
-        fun timeMillis(timeMillis: Long): ApplicationComponentBuilder
-
-        fun build(): ApplicationComponent
+    @Component.Factory
+    interface ApplicationComponentFactory {
+        fun create(
+            @BindsInstance context: Context,
+            @BindsInstance timeMillis: Long
+        ): ApplicationComponent
     }
 }
--- app/src/main/java/com/example/depinjection/example2/presentation/ExampleViewModel.kt
+++ app/src/main/java/com/example/depinjection/example2/presentation/ExampleViewModel.kt
@@ -2,7 +2,9 @@ package com.example.depinjection.example2.presentation
 
 import com.example.depinjection.example2.domain.ExampleUseCase
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class ExampleViewModel @Inject constructor(private val useCase: ExampleUseCase) {
 
     fun method() {

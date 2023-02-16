1cde69457702ccc0c3287a5f51aaea5f0d9f43fb
Qarapapax <aldunsar@yahoo.com>
2023-02-16 14:02:27 +0300

    les10

--- app/src/main/java/com/example/depinjection/example2/data/network/ExampleApiService.kt
+++ app/src/main/java/com/example/depinjection/example2/data/network/ExampleApiService.kt
@@ -4,16 +4,14 @@ import android.content.Context
 import android.util.Log
 import com.example.depinjection.R
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class ExampleApiService @Inject constructor(
     private val context: Context,
     private val timeMillis: Long
 ) {
 
     fun method() {
-        Log.d(LOG_TAG, "ExampleApiService ${context.getString(R.string.app_name)} $timeMillis")
+        Log.d(LOG_TAG, "ExampleApiService ${context.getString(R.string.app_name)} $timeMillis $this")
     }
 
     companion object {
--- app/src/main/java/com/example/depinjection/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/depinjection/example2/di/ApplicationComponent.kt
@@ -1,24 +1,13 @@
 package com.example.depinjection.example2.di
 
 import android.content.Context
-import com.example.depinjection.example2.data.database.ExampleDatabase
-import com.example.depinjection.example2.data.network.ExampleApiService
-import com.example.depinjection.example2.presentation.ExampleViewModel
 import com.example.depinjection.example2.presentation.MainActivity
-import dagger.Binds
 import dagger.BindsInstance
 import dagger.Component
-import javax.inject.Singleton
 
-@Singleton
+@ApplicationScope
 @Component(modules = [DataModule::class, DomainModule::class])
 interface ApplicationComponent {
-
-    fun getViewModel(): ExampleViewModel
-
-    fun getDataBase(): ExampleDatabase
-
-    fun getApiService(): ExampleApiService
 
     fun inject(activity: MainActivity)
 
